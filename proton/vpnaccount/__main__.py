dfc41f5057c04cc096fb1864aff218f9a5f805fc
Mickael Hoerdt <mickael.hoerdt@hesge.ch>
2021-12-28 14:34:43 +0100

    Add test and improved a bit the user example

--- proton/vpnaccount/__main__.py
+++ proton/vpnaccount/__main__.py
@@ -1,29 +1,40 @@
 from proton.sso import ProtonSSO
+from proton.session.api import ProtonAPIAuthenticationNeeded
 from proton.vpnaccount.vpnaccount import VPNAccount, VPNAccountReloadVPNData
 
 def main():
+
     # Create VPN Account object
-    account=VPNAccount()
+    account=VPNAccount('testas1')
+    got_info=False
+
     # Business logic.
     try:
         vpnuser=account.vpn_username
         vpnpass=account.vpn_password
         tier=account.max_tier
+        got_info=True
         print("we got user and password offline!")
     except VPNAccountReloadVPNData:
         sso = ProtonSSO()
         # This only works if you logged in before
         # proton-sso login testas1
         # -> Something to handle at the orchestrator level.
-        account.reload_from_session(sso.get_session('testas1'))
-        vpnuser=account.vpn_username
-        vpnpass=account.vpn_password
-        tier=account.max_tier
-        print('reloaded vpn account to keyring')
-    finally:
+        try:
+            account.reload_from_session(sso.get_session('testas1'))
+            vpnuser=account.vpn_username
+            vpnpass=account.vpn_password
+            tier=account.max_tier
+            got_info=True
+            print('reloaded vpn account info to keyring')
+        except ProtonAPIAuthenticationNeeded:
+            print('please logon on proton API first')
+
+    if got_info:
         print(f'User: {vpnuser}')
         print(f'Pass: {vpnpass}')
         print(f'Tier: {tier}')
+
 
 if __name__=="__main__":
     main()
--- proton/vpnaccount/vpnaccount.py
+++ proton/vpnaccount/vpnaccount.py
@@ -1,7 +1,9 @@
-#Dataclass is a python 3.7 feature
+
+import base64
 from dataclasses import dataclass, fields
 from proton.session.api import Session
 
+#Dataclass is a python 3.7 feature
 @dataclass
 class VPNSettings:
     ExpirationTime: int
@@ -33,7 +35,6 @@ class VPNAccountReloadVPNData(Exception):
     """
 
 class VPNAccount:
-    KEYRING_NAME = "vpnaccount0"
     """
         Wrapper that provides helpers to a persistent offline access to /vpn backend route fields retrieved 
         from the API using the keyring.
@@ -42,16 +43,38 @@ class VPNAccount:
         - If the Info are available through the Keyring, it will be used as an off-line cache.
     """
 
-    def __init__(self):
+    def __init__(self, username:str):
         self._vpn_plan=None
         self._vpn_settings=None
+        self._keyringname=self.__keyring_key_name(username)
         # Load info from the keyring
         keyring = self._keyring
         try:
-            api_vpn_data=keyring[VPNAccount.KEYRING_NAME]
+            api_vpn_data=keyring[self._keyringname]
             self._reload_vpn_data(api_vpn_data)
         except KeyError:
             pass
+
+    # FIXME : taken from proton.sso, should be in a dedicated module
+    def __encode_name(self, account_name) -> str:
+        """Helper function to convert an account_name into a safe alphanumeric string.
+
+        :param account_name: normalized account_name
+        :type account_name: str
+        :return: base32 encoded string, without padding.
+        :rtype: str
+        """
+        return base64.b32encode(account_name.encode('utf8')).decode('ascii').rstrip('=').lower()
+
+    def __keyring_key_name(self, account_name : str) -> str:
+        """Helper function to get the keyring key for account_name
+
+        :param account_name: normalized account_name
+        :type account_name: str
+        :return: keyring key
+        :rtype: str
+        """
+        return f'proton-vpnaccount-{self.__encode_name(account_name)}'
 
     @property
     def _keyring(self) -> "KeyringBackend":
@@ -77,13 +100,20 @@ class VPNAccount:
         # save info to the keyring
         try:
             keyring = self._keyring
-            keyring[VPNAccount.KEYRING_NAME]=api_vpn_data
+            keyring[self._keyringname]=api_vpn_data
         except KeyError:
             pass
 
     def reload_from_session(self, session: Session):
         vpndict = session.api_request('/vpn')
         self._reload_vpn_data(vpndict)
+
+    def clear(self):
+        keyring = self._keyring
+        try:
+            del keyring[self._keyringname]
+        except KeyError:
+            pass
 
     @property
     def vpn_username(self) -> str:
